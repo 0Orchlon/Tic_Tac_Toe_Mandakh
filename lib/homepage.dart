ff1a8d342f8a4cbc8fdc6875db3533c2eedf5ac8
Orchlon <sw22d045@mandakh.edu.mn>
2024-06-06 12:27:58 +0800

    changed the homepages title

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Tic Tac Toe Mandkah'),
       ),
       body: Center(
         child: ElevatedButton(
