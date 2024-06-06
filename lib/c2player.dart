d61a17fc3178a335796fa3261e4b609cbd0812b1
Orchlon <sw22d045@mandakh.edu.mn>
2024-06-06 12:18:38 +0800

    Moved the files to come out and added homepage
    Move the test folders files to the main folder now you can just use "flutter run" to start

---
+++ lib/c2player.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TicTacToeGame(),
+    );
+  }
+}
+
+class TicTacToeGame extends StatefulWidget {
+  @override
+  _TicTacToeGameState createState() => _TicTacToeGameState();
+}
+
+class _TicTacToeGameState extends State<TicTacToeGame> {
+  List<List<String>> board = List.generate(3, (_) => List.filled(3, ''));
+  String currentPlayer = 'X';
+  int moveCount = 0;
+  List<List<int>> moveHistory = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeGame();
+  }
+// start the game
+  void initializeGame() {
+    setState(() {
+      board = List.generate(3, (_) => List.filled(3, ''));
+      currentPlayer = 'X';
+      moveCount = 0;
+      moveHistory.clear();
+    });
+  }
+// place shape
+  void onCellTapped(int row, int col) {
+  if (board[row][col].isEmpty) {
+    setState(() {
+      board[row][col] = currentPlayer;
+      moveCount++;
+      if (moveHistory.length >= 6) {
+        removeFirstMove();
+      }
+      moveHistory.add([row, col]);
+      if (checkWin()) {
+        showWinDialog();
+      } else {
+        currentPlayer = currentPlayer == 'X'? 'O' : 'X';
+      }
+    });
+  }
+}
+// Remove the first shape
+  void removeFirstMove() {
+  if (moveHistory.isNotEmpty) {
+    int row = moveHistory.first[0];
+    int col = moveHistory.first[1];
+    board[row][col] = '';
+    moveHistory.removeAt(0);
+    moveCount--;
+  }
+}
+// Check for win
+  bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+      if (board[i][0]!= '' && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+        return true;
+      }
+      if (board[0][i]!= '' && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+        return true;
+      }
+    }
+    if (board[0][0]!= '' && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+      return true;
+    }
+    if (board[0][2]!= '' && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+      return true;
+    }
+    return false;
+  }
+// Win Dialog
+  void showWinDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Center(child:  Text('Player $currentPlayer wins!'),)
+,          actions: <Widget>[
+            Center(child:
+            ElevatedButton(
+              child: Text('Play Again'),
+              onPressed: () {
+                initializeGame();
+                Navigator.of(context).pop();
+              },
+            )),
+          ],
+        );
+      },
+    );
+  }
+// The Interface
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe Mandkah'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: 9,
+        itemBuilder: (context, index) {
+          final row = index ~/ 3;
+          final col = index % 3;
+          return GestureDetector(
+            onTap: () => onCellTapped(row, col),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Center(
+                child: Text(
+                  board[row][col],
+                  style: TextStyle(fontSize: 48),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'c2player.dart'; // Import the game
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Play'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TicTacToeGame()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
