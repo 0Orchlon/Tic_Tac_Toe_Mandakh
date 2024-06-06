c9878f98d5421d1abc97fa3fa7200b455de850aa
Orchlon <sw22d045@mandakh.edu.mn>
2024-06-06 11:02:12 +0800

    added home page
    turned the main.dart into a home page

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'c2player.dart'; // Import the game
 
 void main() {
   runApp(MyApp());
@@ -8,129 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TicTacToeGame(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class TicTacToeGame extends StatefulWidget {
-  @override
-  _TicTacToeGameState createState() => _TicTacToeGameState();
-}
-
-class _TicTacToeGameState extends State<TicTacToeGame> {
-  List<List<String>> board = List.generate(3, (_) => List.filled(3, ''));
-  String currentPlayer = 'X';
-  int moveCount = 0;
-  List<List<int>> moveHistory = [];
-
-  @override
-  void initState() {
-    super.initState();
-    initializeGame();
-  }
-  void initializeGame() {
-    setState(() {
-      board = List.generate(3, (_) => List.filled(3, ''));
-      currentPlayer = 'X';
-      moveCount = 0;
-      moveHistory.clear();
-    });
-  }
-  void onCellTapped(int row, int col) {
-  if (board[row][col].isEmpty) {
-    setState(() {
-      board[row][col] = currentPlayer;
-      moveCount++;
-      if (moveHistory.length >= 6) {
-        removeFirstMove();
-      }
-      moveHistory.add([row, col]);
-      if (checkWin()) {
-        showWinDialog();
-      } else {
-        currentPlayer = currentPlayer == 'X'? 'O' : 'X';
-      }
-    });
-  }
-}
-  void removeFirstMove() {
-  if (moveHistory.isNotEmpty) {
-    int row = moveHistory.first[0];
-    int col = moveHistory.first[1];
-    board[row][col] = '';
-    moveHistory.removeAt(0);
-    moveCount--;
-  }
-}
-  bool checkWin() {
-    for (int i = 0; i < 3; i++) {
-      if (board[i][0]!= '' && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
-        return true;
-      }
-      if (board[0][i]!= '' && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-        return true;
-      }
-    }
-    if (board[0][0]!= '' && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-      return true;
-    }
-    if (board[0][2]!= '' && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-      return true;
-    }
-    return false;
-  }
-
-  void showWinDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Game Over'),
-          content: Text('Player $currentPlayer wins!'),
-          actions: <Widget>[
-            ElevatedButton(
-              child: Text('Play Again'),
-              onPressed: () {
-                initializeGame();
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tic Tac Toe Mandkah'),
+        title: Text('Home Page'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Play'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TicTacToeGame()),
+            );
+          },
         ),
-        itemCount: 9,
-        itemBuilder: (context, index) {
-          final row = index ~/ 3;
-          final col = index % 3;
-          return GestureDetector(
-            onTap: () => onCellTapped(row, col),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(),
-              ),
-              child: Center(
-                child: Text(
-                  board[row][col],
-                  style: TextStyle(fontSize: 48),
-                ),
-              ),
-            ),
-          );
-        },
       ),
     );
   }
